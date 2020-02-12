d2f32d174da3d43235bd02f42fc216ef714f3aa8
Paco Nathan <ceteri@gmail.com>
2020-02-12 08:02:34 -0800

    needed to fix a path

--- bin/phrase_extraction_pipeline.py
+++ bin/phrase_extraction_pipeline.py
@@ -5,60 +5,71 @@ from parsed_json_interpreter import ParsedJsonInterpreter, mkdir
 from pathlib import Path
 import codecs
 import json
-import logging
 import os
 import pytextrank
 import spacy
 import sys
 
 
-def Setup ():
+def setup (base_path=".", testing=False):
     """
-    add PyTextRank into the spaCy pipeline
+    add PyTextRank into the spaCy pipeline, then set up the input
+    directory path for test vs. production env
     """
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    logger = logging.getLogger("PyTR")
-    
     nlp = spacy.load("en_core_web_sm")
     tr = pytextrank.TextRank(logger=None)
+
     nlp.add_pipe(tr.PipelineComponent, name="textrank", last=True)
 
+    if testing:
+        resource_path = Path(base_path) / "example/pub"
+    else:
+        resource_path = Path(base_path) / "resources/pub"
 
-def PhraseExtraction (resource_path):
+    return nlp, resource_path
+
+
+def extract_phrases (nlp, resource_path, limit_keyphrase=15, verbose=True):
     """
     run PyTextRank on Parsr output to extract and rank key phrases
     """
     json_dir = resource_path / "json"
 
+    if verbose:
+        print(f"scanning input directory: {json_dir}")
+
     for parse_file in list(json_dir.glob("*.json")):
+        if verbose:
+            print(f"loading {parse_file}")
+
         with codecs.open(parse_file, "r", encoding="utf8") as f:
-            object = json.load(f)
+            parsr_object = json.load(f)
        
-        # Parse the JSON and convert it to Txt, devided by sections, and extract the title of the section
-        parsr_interpreter = ParsedJsonInterpreter(object)
-        sections, titles = parsr_interpreter.GetSectionalText(object)
+        # parse the JSON and convert it to text, divided by sections,
+        # then extract the title of each section
+        parsr_interpreter = ParsedJsonInterpreter(parsr_object)
+        sections, titles = parsr_interpreter.GetSectionalText(parsr_object)
         
-        # Now run textrank and save the output 
-        Output = []
+        # run TextRank and collect the ranked keyphrases
+        results = []
 
         for i, section in enumerate(sections):
-            Dict = {}
-            Final = {}
-        
             doc = nlp(section)
+            phrases = {}
+            final = {}
 
-            for phrase in doc._.phrases[:15]:
-                Dict[phrase.text] = {"count": phrase.count, "rank_score": phrase.rank}
+            for phrase in doc._.phrases[:limit_keyphrase]:
+                phrases[phrase.text] = {"count": phrase.count, "rank_score": phrase.rank}
          
-            Final["section_title"] = titles[i]
-            Final["text_rank"] = Dict
-            
-            Output.append(Final)
-            print("\n----------\n")
-            
-        print("Job Done!")
-        
-        # output the rank results to JSON 
+            final["section_title"] = titles[i]
+            final["text_rank"] = phrases
+            results.append(final)
+
+            if verbose:
+                print("section: {}".format(final["section_title"]))
+
+        # output the ranked results to JSON 
+
         tr_path = resource_path / "tr"
         mkdir(tr_path)
 
@@ -66,14 +77,12 @@ def PhraseExtraction (resource_path):
         output_path = tr_path / tr_file
 
         with codecs.open(output_path, "wb", encoding="utf8") as f:
-            json.dump(Output, f, indent=4, ensure_ascii=False)
+            json.dump(results, f, indent=4, ensure_ascii=False)
+
+        if verbose:
+            print(f"completed: {output_path}")
 
                       
 if __name__ == "__main__":
-    Setup()
-
-    # can point to different directories:
-    #resource_path = Path(".") / "example/pub"		# test environment
-    resource_path = Path(".") / "resources/pub"		# production use
-
-    PhraseExtraction(resource_path)
+    nlp, resource_path = setup(testing=True)
+    extract_phrases(nlp, resource_path)
